543a119768c0d44bec899d68bc83624452ea8cd0
Flandre3569 <1332726596@qq.com>
2021-12-07 09:25:42 +0800

    'ES12知识点补充'

---
+++ 19_ES12知识点总结/03_logical_assign_operator.js
@@ -0,0 +1,15 @@
+let n = 1;
+n = n + 1;
+// 可以简写为
+n += 1;
+
+// ||=  --->  逻辑或
+let message = undefined;
+message ||= 'default value';
+//相当于 message = message || 'default value'
+
+// &&=  --->  逻辑与
+
+
+
+// ??=  --->  逻辑空
