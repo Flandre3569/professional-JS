46cf2c0f9564ec4f7efbd3e7e029306f2c35e2e2
Flandre3569 <1332726596@qq.com>
2022-01-19 19:05:02 +0800

    '导入的三种方式'

--- 28_模块化/02_其他使用/main.js
+++ 28_模块化/02_其他使用/main.js
@@ -1,6 +1,14 @@
-import { fName, fAge, fFoo, FPerson } from './foo.js';
+// 第一种导入：普通导入，文件导出的名字是什么，导入的时候名字必须相同
+// import { fName, fAge, fFoo, FPerson } from './foo.js';
 
-fFoo();
-console.log(fName);
-console.log(fAge);
-console.log(FPerson);
+// 第二种导入：导入时起别名
+// import { fName as name, fAge as age, fFoo as foo } from './foo.js';
+
+// 第三种导入：将导出的内容放到一个标识符中
+import * as foo from './foo.js';
+
+
+foo.fFoo();
+console.log(foo.fName);
+console.log(foo.fAge);
+console.log(foo.fFoo);
