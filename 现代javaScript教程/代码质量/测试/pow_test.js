93b017f8ae5f32e7c4bbe6667120de3407ad2388
Flandre3569 <1332726596@qq.com>
2022-07-05 00:10:16 +0800

    [7.5] macho task

--- 现代javaScript教程/代码质量/测试/pow_test.js
+++ 现代javaScript教程/代码质量/测试/pow_test.js
@@ -3,3 +3,41 @@ describe("pow", function () {
     assert.equal(pow(2, 3), 8);
   });
 })
+
+
+describe("pow2", function () {
+  it("test 3 power", function () {
+    assert.equal(pow(3, 2), 9);
+  })
+})
+
+describe("pow", function () {
+
+  function makeTest(x) {
+    let expected = x * x * x;
+    it(`${x} in the power 3 is ${expected}`, function () {
+      assert.equal(pow(x, 3), expected);
+    });
+  }
+
+  for (let x = 1; x <= 5; x++) {
+    makeTest(x);
+  }
+
+});
+
+
+describe("pow", function () {
+  describe("raises x to power 3", function () {
+    function makeTest(x) {
+      let expected = x * x * x;
+      it(`${x} in the power 3 is ${expected}`, function () {
+        assert.equal(pow(x, 3), expected);
+      })
+    }
+
+    for (let x = 1; x < 5; x++) {
+      makeTest(x);
+    }
+  })
+})
